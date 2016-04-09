6294dbb183fa16bedaedbd37a96dce3a46033d3a
Eddie Rodriguez <rodriguez74@gmail.com>
2016-04-09 19:44:46 -0400

    Saturday Commit

--- madlib with loops/js/madlib-console.js
+++ madlib with loops/js/madlib-console.js
@@ -1,8 +1,11 @@
 var startupX = ['Uber', 'Google', 'Amazon', 'Apple', 'Facebook', 'Twitter'];
 var startupY = ['Slack', 'Trello', 'Tesla', 'Hyperloop', 'Harvest'];
- document.addEventListener("click", function(event1){
- document.getElementById("xForY").innerHTML = [startupX];
+ document.getElementById("create").addEventListener("click", function(){
+var random1 = Math.floor((Math.random() * startupX.length));
+var random2 = Math.floor((Math.random() * startupY.length));
+ document.getElementById("xForY").innerHTML =  'A startup that is ' + startupX[random1] + ', but for ' + startupY[random2];
 });
+
 var random1 = Math.floor((Math.random() * startupX.length));
 var random2 = Math.floor((Math.random() * startupY.length));
 
